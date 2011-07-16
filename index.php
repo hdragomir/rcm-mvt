6317935ef9a9dd3031a5813b359d47f6c73cfdde
Horia Dragomir <horia@hdragomir.com>
2011-07-16 04:01:55 +0300

    added footer

--- index.php
+++ index.php
@@ -43,6 +43,32 @@
 </section>
 
 
+<footer>
+
+    <div class="top cc">
+        <section id="partners" class=ir></section>
+    </div><div class="bottom">
+
+        <div class="cc">
+
+            <a href="http://www.facebook.com/pages/Rugby-Club-Timisoara-Pagina-oficiala/224404584253969" rel="me external" id="to-fb" class=ir>facebook </a>
+
+
+            <?php wp_nav_menu(array('menu' => 'nav', 'theme_location' => 'Navigation', 'depth' => 2, 'container' => 'nav', 'menu_class' => 'dd', 'menu_id' => 'footernav', 'walker' => new extended_walker())); ?>
+
+
+            <div id="footercopy">© RCM MVT Universitatea de Vest Timisoara 2011. Toate drepturile rezervate.</div>
+            <div id="footermisc"><a href="<?php to_page('contact'); ?>">Contact</a> | <a href="http://rugby-timisoara.ro/webmail" rel=external>Login</a></div>
+
+        </div>
+
+
+    </div>
+
+
+</footer>
+
+
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 	<script>!window.jQuery && document.write(unescape('%3Cscript src="js/libs/jquery-1.5.1.min.js"%3E%3C/script%3E'))</script>
 	<script src="js/script.js"></script>
