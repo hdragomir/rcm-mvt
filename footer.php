0c007938307e7293f381cf6fe0075c5eaac11287
Horia Dragomir <horia@hdragomir.com>
2011-07-30 15:50:34 +0200

    removed Parteneri/Sponsori header

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
 
 <footer>
 
-    <div class="top cc">
-        <h1>Parteneri / Sponsori</h1>
+    <div class="top cc"><s></s>
         <section id="partners" class=ir></section>
     </div><div class="bottom">
 
