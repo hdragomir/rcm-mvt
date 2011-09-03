f8b420951488d6338c6b7d2c6ec465b1c55f37d8
Horia Dragomir <horia@hdragomir.com>
2011-09-03 19:15:00 +0200

    added google Chrome Frame

--- footer.php
+++ footer.php
@@ -52,5 +52,11 @@
   })();
 
     </script>
+    
+<!--[if lt IE 8 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
+    <script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+<![endif]-->
+
 </body>
 </html>
