3321a72546e4896011b2e3dd639185af291a950c
Horia Dragomir <horia@hdragomir.com>
2011-09-15 23:10:01 +0200

    busted cache for stylesheet

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?v=1.3">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?v=1.4">
 
 	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr-1.7.min.js"></script>
 </head>
