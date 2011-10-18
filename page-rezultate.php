2ea6c470c06a56838a30149035d654f58a18d33b
Horia Dragomir <horia@hdragomir.com>
2011-10-18 23:29:39 +0200

    shortened the date in the results table

--- page-rezultate.php
+++ page-rezultate.php
@@ -55,7 +55,7 @@
                 ?>
                 <tr>
                         <td class="step"><?php echo get_post_meta($match->ID, 'etapa', true); ?></td>
-                        <td class=when><?php echo apply_filters('the_date', date('d F Y, \O\r\a H:i', strtotime($match->post_date))); ?></td>
+                        <td class=when><?php echo apply_filters('the_date', date('d.m.Y, \O\r\a H:i', strtotime($match->post_date))); ?></td>
                         <td class=teams><?php echo $at_home? $us : $them; ?> <br> <?php echo $at_home? $them : $us;
 
                         if($news_ID):
